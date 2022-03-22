5488b7932f76e8f72b35814d6312f94c2b9d59eb
HarshalBhole <hb32@njit.edu>
2022-03-22 03:04:20 -0400

    signup login page design modified

--- src/components/SigninPage/SigninPage.js
+++ src/components/SigninPage/SigninPage.js
@@ -1,85 +1,81 @@
 import './SigninPage.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import $ from 'jquery';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import news from '../../Assets/news.json';
 
 /**
- * 
- * @returns Signin form component 
+ *
+ * @returns Signin form component
  * Details: Component that allows the user to enter
  *          details for a user that is already in the database
  *          and validate whether they can login
- * 
+ *
  */
 function SigninPage() {
-
-  const [passerror,setPassError] = useState("")
-  const [usererror,setUserError] = useState("")
+  const [passerror, setPassError] = useState('');
+  const [usererror, setUserError] = useState('');
   const navigate = useNavigate();
 
   return (
-    <div className="signin">
-      <section className="left-section">
-        <div id="left-form" className="form">
+    <div className='signin'>
+      <section className='left-section'>
+        {/* <div className='signuptxtcontainer'>
+          <div className='mainpage' onClick={goSignup}>
+            {`< Go to Main Page`}
+          </div>
+        </div> */}
+        <Lottie animationData={news} style={{ width: '40%' }} loop={true} />
+        <div id='left-form' className='form'>
           <h1>Login</h1>
-          <div className = "innerform">
+          <div className='innerform'>
             <div className='userin_div'>
               <input
-                id="userinput"
-                type="text"
-                name="user-name"
-                className="input-box"
-                placeholder="User Name"
+                id='userinput'
+                type='text'
+                name='user-name'
+                className='input-box'
+                placeholder='User Name'
                 onClick={resetErr}
               />
-              {
-                usererror !== "" &&
-                <label htmlFor="user-name">{usererror}</label>
-              }
-             
-
+              {usererror !== '' && (
+                <label htmlFor='user-name'>{usererror}</label>
+              )}
             </div>
             <div className='passin_div'>
               <input
-                id="passinput"
-                type="password"
-                name="user-pass"
-                className="input-box"
-                placeholder="Password"
+                id='passinput'
+                type='password'
+                name='user-pass'
+                className='input-box'
+                placeholder='Password'
                 onClick={resetErr}
               />
-              {
-                passerror !== "" &&
-                <label id = "passerr_lbl" htmlFor="user-pass">{passerror}</label>
-              }
-              
-              
+              {passerror !== '' && (
+                <label id='passerr_lbl' htmlFor='user-pass'>
+                  {passerror}
+                </label>
+              )}
             </div>
-            
-            <button
-                name="login-btn"
+            <div className='loginbtnContainer'>
+              <button
+                name='login-btn'
                 onClick={checkSignin}
-                className="btn"
-              >Login</button>
-            <div className = "navbtns">
-              
-              <button
-                      id = "submitbtn"
-                      name="signup-btn"
-                      onClick={goSignup}
-                      className="btn"
-                      
-              >Signup?</button>
-              <button
-                      id = "submitbtn"
-                      name="signup-btn"
-                      onClick={goMainPage}
-                      className="btn"
-                      
-              >Main Page</button>
-
+                className='loginbtn'
+              >
+                Login
+              </button>
             </div>
-            
+            <div className='navbtns'>
+              <div className='signuptxtcontainer'>
+                <div className='signuptxt'>New here?</div>
+                <div className='signuplink' onClick={goSignup}>
+                  Signup now
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -87,59 +83,58 @@ function SigninPage() {
   );
 
   //resets the error message state variables
-  function resetErr(){
-    setPassError("")
-    setUserError("")
+  function resetErr() {
+    setPassError('');
+    setUserError('');
   }
 
   //on clicking the signup button redirect to signup page
-  function goSignup(){
-    navigate('/signup', {state:{name:""}});
+  function goSignup() {
+    navigate('/signup', { state: { name: '' } });
   }
 
   //on clicking the main page button redirect to main page
-  function goMainPage(){
-    navigate('/', {state:{name:""}});
+  function goMainPage() {
+    navigate('/', { state: { name: '' } });
   }
 
   //function that calls the signin api and checks whether user is valid
   //if not sets appropriate error message
-  function checkSignin(){
-    const userinput = document.getElementById("userinput").value;
-    const passinput = document.getElementById("passinput").value;
-    
-    const data = {username:userinput, password:passinput};
-    
-    
+  function checkSignin() {
+    const userinput = document.getElementById('userinput').value;
+    const passinput = document.getElementById('passinput').value;
+
+    const data = { username: userinput, password: passinput };
+
     $.ajax({
       //url: 'https://gentle-island-18820.herokuapp.com/api/signin',
       url: 'http://127.0.0.1:5000/api/signin',
       type: 'POST',
       crossorigin: true,
-      cache:false,
-      dataType: "json",
-      contentType: "application/json",
-      credentials: "include",
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json',
+      credentials: 'include',
       xhrFields: {
-        withCredentials: true
+        withCredentials: true,
       },
-      data:JSON.stringify(data),
-      success: function(data,xhr){
-        setPassError("");
-        setUserError("");
-        navigate('/', {state:{name:userinput}});
+      data: JSON.stringify(data),
+      success: function (data, xhr) {
+        setPassError('');
+        setUserError('');
+        navigate('/', { state: { name: userinput } });
       },
-      error: function(data,request,error){
-        if(data.responseJSON.user_error === "true"){
-          setUserError("Incorrect Username");
+      error: function (data, request, error) {
+        if (data.responseJSON.user_error === 'true') {
+          setUserError('Incorrect Username');
         }
-        
-        if(data.responseJSON.pass_error === "true"){
-          setPassError("Incorrect Password");
+
+        if (data.responseJSON.pass_error === 'true') {
+          setPassError('Incorrect Password');
         }
-      }
-    })
-  };
+      },
+    });
+  }
 }
 
 export default SigninPage;
--- src/components/SignupPage/SignupPage.js
+++ src/components/SignupPage/SignupPage.js
@@ -1,274 +1,264 @@
 import './SignupPage.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import $ from 'jquery';
-import React,{useState} from 'react';
-
+import React, { useState } from 'react';
+import Lottie from 'lottie-react';
+import news from '../../Assets/news.json';
 /**
- * 
- * @returns signup form component 
+ *
+ * @returns signup form component
  * Details: Component that allows the user to enter
  *          details for a new user and save them in a database
- * 
+ *
  */
 function SignupPage() {
-  
-  const [user_error, setUser] = useState("");
-  const [pass1, setPass1] = useState("");
-  const [pass2, setPass2] = useState("");
-  const [pass3, setPass3] = useState("");
-  const [pass4, setPass4] = useState("");
-  const [pass5, setPass5] = useState("");
-  const [confirm, setConfirm] = useState("");
+  const [user_error, setUser] = useState('');
+  const [pass1, setPass1] = useState('');
+  const [pass2, setPass2] = useState('');
+  const [pass3, setPass3] = useState('');
+  const [pass4, setPass4] = useState('');
+  const [pass5, setPass5] = useState('');
+  const [confirm, setConfirm] = useState('');
   const navigate = useNavigate();
 
   return (
-    <div className="signup">
-        <section className="right-section">
-          <div id="right-form" className="form">
-            <h1>Signup</h1>
-            <div className = "innerform">
-              <div className = "userin">
-                <input
-                  id="userinput"
-                  type="text"
-                  name="user-name"
-                  className="input-box"
-                  placeholder="User Name"
-                  onClick={resetErrors}
-                />
-                {user_error !== "" && <label htmlFor="user-name">{user_error}</label>}
-              </div>
-              <div className = "passin">
-                <input
-                  id="passinput"
-                  type="password"
-                  name="user-pass"
-                  className="input-box"
-                  placeholder="Password"
-                  onClick={resetErrors}
-                />
-                {
-                  pass1 !== "" &&
-                  <div className = "pass_err_div">
-                    {pass1 !== "" && <label htmlFor="user-pass">{pass1}</label>}
-                  </div>
-                }
-                {
-                  pass2 !== "" &&
-                  <div className = "pass_err_div">
-                    {pass2 !== "" && <label htmlFor="user-pass">{pass2}</label>}
-                  </div>
-                }
-                {
-                  pass3 !== "" &&
-                  <div className = "pass_err_div">
-                    {pass3 !== "" && <label htmlFor="user-pass">{pass3}</label>}
-                  </div>
-                }
-              </div>
-              
-              <div className='confirm_div'>
-                <input
-                  id="confirmpass"
-                  type="password"
-                  name="user-cpass"
-                  className="input-box"
-                  placeholder="Confirm Password"
-                  onClick={resetErrors}
-                />
-                {pass4 !== "" && <label htmlFor="user-pass">{pass4}</label>}
-                {pass5 !== "" && <label htmlFor="user-pass">{pass5}</label>}
-              </div>
+    <div className='signup'>
+      <section className='right-section'>
+        <Lottie animationData={news} style={{ width: '40%' }} loop={true} />
 
-              <div className = "navbtns">
-                <button
-                    id =  "homebtn"
-                    name="signup-btn"
-                    onClick={goSignin}
-                    className="btn"  
-                  >Login?</button>
-                  <button
-                    id = "submitbtn"
-                    name="signup-btn"
-                    onClick={checkSignup}
-                    className="btn"
-                    
-                  >Signup</button>
-                  
+        <div id='right-form' className='form'>
+          <h1>Signup</h1>
+          <div className='innerform'>
+            <div className='userin'>
+              <input
+                id='userinput'
+                type='text'
+                name='user-name'
+                className='input-box'
+                placeholder='User Name'
+                onClick={resetErrors}
+              />
+              {user_error !== '' && (
+                <label htmlFor='user-name'>{user_error}</label>
+              )}
+            </div>
+            <div className='passin'>
+              <input
+                id='passinput'
+                type='password'
+                name='user-pass'
+                className='input-box'
+                placeholder='Password'
+                onClick={resetErrors}
+              />
+              {pass1 !== '' && (
+                <div className='pass_err_div'>
+                  {pass1 !== '' && <label htmlFor='user-pass'>{pass1}</label>}
+                </div>
+              )}
+              {pass2 !== '' && (
+                <div className='pass_err_div'>
+                  {pass2 !== '' && <label htmlFor='user-pass'>{pass2}</label>}
+                </div>
+              )}
+              {pass3 !== '' && (
+                <div className='pass_err_div'>
+                  {pass3 !== '' && <label htmlFor='user-pass'>{pass3}</label>}
+                </div>
+              )}
+            </div>
+
+            <div className='confirm_div'>
+              <input
+                id='confirmpass'
+                type='password'
+                name='user-cpass'
+                className='input-box'
+                placeholder='Confirm Password'
+                onClick={resetErrors}
+              />
+              {pass4 !== '' && <label htmlFor='user-pass'>{pass4}</label>}
+              {pass5 !== '' && <label htmlFor='user-pass'>{pass5}</label>}
+            </div>
+
+            <div className='navbtns'>
+              <button
+                id='submitbtn'
+                name='signup-btn'
+                onClick={checkSignup}
+                className='btn'
+              >
+                Signup
+              </button>
+            </div>
+            <div className='navbtns'>
+              <div className='signuptxtcontainer'>
+                <div className='signuptxt'>Already a user ? </div>
+                <div className='signuplink' onClick={goSignin}>
+                  Login
+                </div>
               </div>
-              
             </div>
           </div>
-          {
-            confirm && confirm !== "" && 
-            <div className = "usercreate">{confirm}</div>
-          }
-        </section>
-        
+        </div>
+        {confirm && confirm !== '' && (
+          <div className='usercreate'>{confirm}</div>
+        )}
+      </section>
     </div>
-    
   );
 
   //resets all error message to default values
-  function resetErrors(){
-    setUser("");
-    setPass1("");
-    setPass2("");
-    setPass3("");
-    setPass4("");
-    setPass5("");
-    setConfirm("");
+  function resetErrors() {
+    setUser('');
+    setPass1('');
+    setPass2('');
+    setPass3('');
+    setPass4('');
+    setPass5('');
+    setConfirm('');
   }
 
   //on clicking signin button go to signin page
-  function goSignin(){
-    navigate('/signin', {state:{name:""}});
+  function goSignin() {
+    navigate('/signin', { state: { name: '' } });
   }
 
-  //function that calls the signup api 
-  function callApi(url,datain){
+  //function that calls the signup api
+  function callApi(url, datain) {
     $.ajax({
       //url: 'https://gentle-island-18820.herokuapp.com/api/signup',
       url: 'http://127.0.0.1:5000/api/signup',
       //url: url,
-      type:'POST',
-      crossorigin:true,
-      cache:false,
-      dataType:"json",
-      contentType: "application/json",
-      data:JSON.stringify(datain),
-      success: function(data){
-        if(data.data_out === "true"){
-          setConfirm("User successfully created");
+      type: 'POST',
+      crossorigin: true,
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(datain),
+      success: function (data) {
+        if (data.data_out === 'true') {
+          setConfirm('User successfully created');
         }
       },
-      error: function(request,error){
-        setConfirm("");
-        console.log(error)
-      }
+      error: function (request, error) {
+        setConfirm('');
+        console.log(error);
+      },
     });
-  };
+  }
 
   //check if password contains a lowercase character
-  function checkLowerChar(password){
+  function checkLowerChar(password) {
     let k = 0;
 
-    for(let i = 0; i < password.length;i++){
-      if(password[i] === password[i].toLowerCase()){
+    for (let i = 0; i < password.length; i++) {
+      if (password[i] === password[i].toLowerCase()) {
         k = 1;
       }
     }
-    if(k === 1){
+    if (k === 1) {
       return true;
-    } 
-    else{return false}
+    } else {
+      return false;
+    }
   }
 
   //check if password contains an uppercase character
-  function checkUpperChar(password){
+  function checkUpperChar(password) {
     let k = 0;
-    for(let i = 0; i < password.length ;i++){
-      if(password[i] === password[i].toUpperCase()){
+    for (let i = 0; i < password.length; i++) {
+      if (password[i] === password[i].toUpperCase()) {
         k = 1;
       }
     }
-    if(k === 1){
+    if (k === 1) {
       return true;
-    } 
-    else{
-      return false
+    } else {
+      return false;
     }
   }
 
   //check if password contains a non letter
-  function checkNotLetter(password){
+  function checkNotLetter(password) {
     let k = 0;
     let reg = /[a-zA-Z]/;
-    for(let i = 0; i < password.length;i++){
-      if(!(password[i].match(reg))){
-        k = 1
+    for (let i = 0; i < password.length; i++) {
+      if (!password[i].match(reg)) {
+        k = 1;
       }
     }
-    if(k === 1){
-      return true
-    }
-    else{
-      return false
+    if (k === 1) {
+      return true;
+    } else {
+      return false;
     }
   }
 
   //check if the given string is greater than or equal to a given length
-  function checkLength(stringIn, length){
-    if(stringIn.length >= length){
+  function checkLength(stringIn, length) {
+    if (stringIn.length >= length) {
       return true;
-    }
-    else{
-      return false
+    } else {
+      return false;
     }
   }
 
-  //main function that tests userinput and passwords against 
+  //main function that tests userinput and passwords against
   //input contstraints and only allows valid username and password format
-  function checkSignup(){
+  function checkSignup() {
+    let url = 'https://gentle-island-18820.herokuapp.com/api/signup';
+    let error = false;
+    let userinput = document.getElementById('userinput').value;
+    let passinput = document.getElementById('passinput').value;
+    let confirmpass = document.getElementById('confirmpass').value;
 
-      let url = 'https://gentle-island-18820.herokuapp.com/api/signup'
-      let error = false
-      let userinput = document.getElementById('userinput').value;
-      let passinput = document.getElementById('passinput').value;
-      let confirmpass = document.getElementById('confirmpass').value;
+    userinput = userinput.replaceAll(' ', '');
+    passinput = passinput.replaceAll(' ', '');
+    confirmpass = confirmpass.replaceAll(' ', '');
 
-      userinput = userinput.replaceAll(' ', '')
-      passinput = passinput.replaceAll(' ', '')
-      confirmpass = confirmpass.replaceAll(' ', '')
-      
+    resetErrors();
 
-      resetErrors();
-      
-      if(checkLength(userinput,8) === false){
-        setUser("Error - Username does not contain at least 8 characters")
-        error = true
-      }
+    if (checkLength(userinput, 8) === false) {
+      setUser('Error - Username does not contain at least 8 characters');
+      error = true;
+    }
 
-      if(checkLength(passinput,8) === false){
-        setPass1("Error - Password does not contain at least 8 characters");
-        
-        error = true
-      }
+    if (checkLength(passinput, 8) === false) {
+      setPass1('Error - Password does not contain at least 8 characters');
 
-      
-      
-      if(checkNotLetter(passinput) === false){
-        setPass2("Error - Password does not contain at least one non letter")
-        error = true
+      error = true;
+    }
+
+    if (checkNotLetter(passinput) === false) {
+      setPass2('Error - Password does not contain at least one non letter');
+      error = true;
+    }
+
+    if (checkUpperChar(passinput) === false) {
+      setPass3('Error - Password does not contain an uppercase character');
+      error = true;
+    }
+
+    if (checkLowerChar(passinput) === false) {
+      setPass4('Error - Password does not contain a lowercase character');
+      error = true;
+    }
+
+    if (!(passinput === confirmpass)) {
+      setPass5('Error - Passwords do not match or are blank');
+      error = true;
+    } else {
+      if (passinput === confirmpass && passinput === '') {
+        setPass5('Error - Passwords do not match or are blank');
       }
-            
-      if(checkUpperChar(passinput) === false){
-        setPass3("Error - Password does not contain an uppercase character")
-        error = true
-      }
-      
-      if(checkLowerChar(passinput) === false){
-        setPass4("Error - Password does not contain a lowercase character")
-        error = true
-      }
-      
-      
-      if(!(passinput === confirmpass)){
-        setPass5("Error - Passwords do not match or are blank")
-        error = true
-      }
-      else{
-        if((passinput === confirmpass) && passinput===""){
-          setPass5("Error - Passwords do not match or are blank")
-        }
-      }
-     
-      if(error === false){
-        let data = {username:userinput, password:passinput};
-        callApi(url, data)
-      }
-      
-  };
+    }
+
+    if (error === false) {
+      let data = { username: userinput, password: passinput };
+      callApi(url, data);
+    }
+  }
 }
 
 export default SignupPage;
